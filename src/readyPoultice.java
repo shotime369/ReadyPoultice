cff8ffc9eeba54007b50d45661b1bbe6a657dbbe
Shotime369 <27260255@nescol.ac.uk>
2023-12-08 18:57:18 +0000

    project cards

--- src/readyPoultice.java
+++ src/readyPoultice.java
@@ -12,6 +12,8 @@ public class readyPoultice {
     static String descriptor;
     static String characterName;
     static String characterID;
+    static int carry;
+    static int speed;
 
     public static String getCardNumber(int cardValue){
         if (cardValue == 1){
@@ -42,6 +44,11 @@ public class readyPoultice {
         return suitSymbol;
     }
     public static void printCard() {
+        Random random = new Random();
+        cardValue = random.nextInt(1, 14);
+        cardSuit = random.nextInt(0, 4);
+        suitSymbol = getCardSuit(cardSuit);
+        cardNumber = getCardNumber(cardValue);
         if (cardValue == 10) {
             System.out.format("""
                     \uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44
@@ -52,7 +59,8 @@ public class readyPoultice {
                      \uD83C\uDF3F║         ║\uD83C\uDF3F
                      \uD83C\uDF44║      %s ║\uD83C\uDF44
                      \uD83C\uDF3F╚═════════╝\uD83C\uDF3F
-                     \uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44""", readyPoultice.cardNumber, suitSymbol, readyPoultice.cardNumber);
+                     \uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44
+                    """, readyPoultice.cardNumber, suitSymbol, readyPoultice.cardNumber);
         } else {
             System.out.format("""
                     \uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44
@@ -63,15 +71,9 @@ public class readyPoultice {
                     \uD83C\uDF3F║         ║\uD83C\uDF3F
                     \uD83C\uDF44║       %s ║\uD83C\uDF44
                     \uD83C\uDF3F╚═════════╝\uD83C\uDF3F
-                    \uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44""", readyPoultice.cardNumber, suitSymbol, readyPoultice.cardNumber);
+                    \uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44\uD83C\uDF3F\uD83C\uDF44
+                    """, readyPoultice.cardNumber, suitSymbol, readyPoultice.cardNumber);
         }
-    }
-    public static void pullCard(){
-        Random random = new Random();
-        cardValue = random.nextInt(1, 14);
-        cardSuit = random.nextInt(0, 4);
-        suitSymbol = getCardSuit(cardSuit);
-        cardNumber = getCardNumber(cardValue);
     }
 
     public static void speciesOptions(){
@@ -158,13 +160,14 @@ public class readyPoultice {
     }
     public static void happy() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Select your option: ");
-        int option1 =  sc.nextInt();
+
         System.out.println("""
                                     Continue (1)\s
                                     Start New Character (2)\s
                                     Main Menu (3)\s
                 """);
+        System.out.print("Select your option: ");
+        int option1 =  sc.nextInt();
         switch (option1) {
             case 1 -> {
             }
@@ -172,29 +175,72 @@ public class readyPoultice {
                 makeCharacter();
                 happy();
             }
-            case 3 -> {
-                System.out.println("***Main Menu***");
-            }
+            case 3 -> System.out.println("***Main Menu***");
         }
     }
-    public static void makeCharacter(){
+    public static void makeCharacter() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Who are you?");
-        System.out.println("Draw a card to choose your species...");
+        System.out.println("Press enter to draw a card: ");
         scanner.nextLine();
-        pullCard();
         printCard();
-        System.out.println("\n");
         speciesOptions();
         Scanner input = new Scanner(System.in);
         System.out.format("\nEnter your species from the list above or choose your own %s animal: ", descriptor);
         characterSpecies = input.nextLine();
-        System.out.format("\nPlease choose a name for your %s : ",characterSpecies);
-        characterName= input.nextLine();
-        System.out.format("\nYour Poulticier is %s %s\n",characterName,characterSpecies);
-        System.out.println("How do you travel?");
-        System.out.println("Draw a card to choose your species...");
+        // add error handling do not allow blank name or species *****************
+        System.out.format("\nPlease choose a name for your %s : ", characterSpecies);
+        characterName = input.nextLine();
+        System.out.format("\nYour Poulticier is %s %s\n", characterName, characterSpecies);
+        System.out.println("\nHow do you travel?");
+        System.out.println("Carry determines how many items like tools and reagents you can hold. Speed affects how far you can travel when Moving.");
+        System.out.print("Press enter to draw a card: ");
         scanner.nextLine();
+        printCard();
+        String travelStyle = ("");
+        switch (cardSuit) {
+            case 0 -> {
+                travelStyle = ("Slow and Steady");
+                speed = 2;
+                carry = 5;
+            }
+            case 1, 2 -> {
+                travelStyle = ("Rambling and Ready");
+                speed = 3;
+                carry = 4;
+            }
+            case 3 -> {
+                travelStyle = ("Fast and Heady");
+                speed = 4;
+                carry = 3;
+            }
+        }
+        System.out.format("\n%s Travel style is %s- Speed %d, Carry %d",suitSymbol, travelStyle, speed, carry);
+        System.out.println("\nAlternatively you may pick Swift and Soaring - Speed 5, Carry 2");
+        //add choice here*************
+        System.out.println("\nHow do you start out?");
+        System.out.println("A dangerous world makes most beasts practical. Our origins don't often start with idle dreams but calls to action. Who (or what) first helped you onto your path as a Poulticepounder?");
+        System.out.print("Press enter to draw a card: ");
+        scanner.nextLine();
+        printCard();
+        String origin = ("");
+        switch (cardSuit) {
+            case 0 -> {
+                origin = (" Inspired by a passing Poulticier, and the impression they left.");
+            }
+            case 1 -> {
+                origin = (" Recruited to help elderly Poulticiers find reagents.");
+            }
+            case 2 -> {
+                origin = (" Recieved treatment after a violent encounter.");
+            }
+            case 3 -> {
+                origin = (" Needed a Poulticier's services after an accident.");
+            }
+        }
+        System.out.format(suitSymbol + origin);
+
+
     }
 
     public static void main(String[] args) throws IOException {
