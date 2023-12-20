2fc88626521d300871afb328e2c165a55a388fe0
shotime369 <27260255@nescol.ac.uk>
2023-12-20 11:26:45 +0000

    working on it

--- src/readyPoultice.java
+++ src/readyPoultice.java
@@ -84,177 +84,152 @@ public class readyPoultice {
     public static void setFamiliarRelationship(){
     switch (cardValue){
         case 1 -> familiarRelationship = """
-                Chanced Upon. Until recently, you had no idea who
-                    they were. Now, you get on like a house on fire.
-                        Journal about the spark that started your
-                        relationship’s blaze.""";
+                Chanced Upon.\s
+                💐Until recently, you had no idea who they were. Now, you get on like a house on fire.
+                ✏ Journal about the spark that started your relationship’s blaze.""";
         case 2 -> familiarRelationship = """
-    Companion. Your shared love runs deep. You oft
-    think about each other and feel stronger together.
-    Journal about when you realised you loved them.""";
+                Companion.\s
+                💐Your shared love runs deep. You oft think about each other and feel stronger together.
+                 ✏ Journal about when you realised you loved them.""";
         case 3 -> familiarRelationship = """
-                Collaborators. You were united by a passion for
-                        seeing a project or cause through to the end.
-                        Journal about what you both hope to achieve.""";
+                Collaborators.\s
+                💐You were united by a passion for seeing a project or cause through to the end.
+                ✏ Journal about what you both hope to achieve.""";
         case 4 -> familiarRelationship = """
-                Mentor. It can be annoying to have your reins pulled
-                        in, but this wiser beast’s experience is invaluable.
-                        Journal about the most important lesson they have
-                        imparted to you.""";
+                Mentor.\s
+                💐It can be annoying to have your reins pulled in, but this wiser beast’s experience is invaluable.
+                ✏ Journal about the most important lesson they have imparted to you.""";
         case 5 -> familiarRelationship = """
-                Supportive. Something in common hounds you
-                        both. You’ve found security in each other’s paws.
-                        Journal about what you are running from.""";
+                Supportive.\s
+                💐Something in common hounds you both. You’ve found security in each other’s paws.
+                ✏ Journal about what you are running from.""";
         case 6 -> familiarRelationship = """
-                Wildcard. Something about them absolutely
-                        fascinates you—they find this entertaining.
-                        Journal about why they are so cool!""";
+                Wildcard.\s
+                💐Something about them absolutely fascinates you—they find this entertaining.
+                Journal about why they are so cool!""";
         case 7 -> familiarRelationship = """
-                Oldest Friend. You grew up together and know
-                        everything about each other. Who else could you trust?
-                        Journal about something embarrassing they did as
-                        a cub, pup, fledgling or hatchling""";
+                Oldest Friend.\s
+                💐You grew up together and know everything about each other. Who else could you trust?
+                ✏ Journal about something embarrassing they did as a cub, pup, fledgling or hatchling""";
         case 8 -> familiarRelationship = """
-                Blood-bound. You agreed never to speak of it again.
-                        This secret will be kept from here to Elsewhere.
-                        Think about what happened. Never admit to it.""";
+                Blood-bound.\s
+                You agreed never to speak of it again.
+                This secret will be kept from here to Elsewhere.
+                Think about what happened. Never admit to it.""";
         case 9 -> familiarRelationship = """
-                Sibling. It doesn't matter if you actually share
-                        blood; you treat this rapscallion as your own family.
-                        It's just a shame they can be so frustrating at
-                        times! Will they ever grow up?\s
-                        Journal about a lesson they keep failing to learn.""";
+                Sibling.\s
+                💐It doesn't matter if you actually share blood; you treat this rapscallion as your own family.
+                It's just a shame they can be so frustrating at times! Will they ever grow up?\s
+                ✏ Journal about a lesson they keep failing to learn.""";
         case 10 -> familiarRelationship = """
-                Rekindled. You once shared a very different
-                        relationship. What matters presently is that you
-                        need each other.\s
-                        Journal about the breakup, and
-                        crossing paths again.""";
+                Rekindled.\s
+                💐You once shared a very different relationship.\s
+                What matters presently is that you need each other.\s
+                ✏ Journal about the breakup, and crossing paths again.""";
         case 11 -> familiarRelationship = """
-                Elsewhere. They’re gone, you’ll never see them
-                        again. Still, they’re in your mind and memories, and
-                        sometimes you talk.
-                        Journal about something or somewhere you wish
-                        you could have shown them.""";
+                Elsewhere.\s
+                💐They’re gone, you’ll never see them again.\s
+                Still, they’re in your mind and memories, and sometimes you talk.
+                ✏ Journal about something or somewhere you wish you could have shown them.""";
         case 12, 13 -> familiarRelationship = """
-                Parental. They nursed you from a tiny thing to a
-                        fully-fledged adult. Even though you’ve flown the
-                        nest, they still protectively circle you.
-                        Journal about the stifling safety they provide.""";
-
-
+                Parental.\s
+                💐They nursed you from a tiny thing to a fully-fledged adult.\s
+                Even though you’ve flown the nest, they still protectively circle you.
+                ✏ Journal about the stifling safety they provide.""";
     }
-
+        System.out.println(familiarRelationship);
 }
     public static void familiarOptions(){
 
         switch (cardValue){
             case 1 -> {
-                descriptor2 = "Brushwise. They grew up amongst brambles and \n" +
-                        "dandelions, and know many Bristley Woods plants.";
+                descriptor2 = """
+                        Brushwise.\s
+                        They grew up amongst brambles and dandelions, and know many Bristley Woods plants.""";
                 benefit = ("-2 Rarity for all Plant Reagents.");
-
             }
             case 2 -> {
                 descriptor2 = """
-                        Helpful. They are full of a plethora of remedy
-                        making tips. Their incredible bedside manner\s
-                        soothes even the surliest beasts.""";
+                        Helpful.\s
+                        They are full of a plethora of remedy making tips.\s
+                        Their incredible bedside manner soothes even the surliest beasts.""";
                 benefit = ("Start all Ailment Timers with +2.");
-
             }
             case 3 -> {
                 descriptor2 = """
-                        Brave. Despite their comparatively small size,\s
-                        they fiercely face larger threats and have a host of\s
-                        daring stories.""";
+                        Brave.\s
+                        Despite their comparatively small size,\s
+                        they fiercely face larger threats and have a host of daring stories.""";
                 benefit = ("""
-                        When you draw a ♥ or ♦ for a Travel Encounter with\s
-                        a Behemoth tag h, gain a local Reagent (rarity ≤6)\s
-                        and end the event positively.""");
-
+                        When you draw a ♥ or ♦ for a Travel Encounter with a Behemoth,\s
+                        gain a local Reagent (rarity ≤6) and end the event positively.""");
             }
             case 4 ->{
                 descriptor2 = """
-                        Chatty. They could talk the ears off of a long
-                        eared brown bat! They are an interesting travel\s
-                        companion with a knack for gathering gossip""";
-                benefit = ("When Bartering, -2 Base Rarity for the desired \n" +
-                        "Reagent Part");
+                        Chatty.\s
+                        They could talk the ears off of a long eared brown bat!\s
+                        They are an interesting travel companion with a knack for gathering gossip""";
+                benefit = ("When Bartering, -2 Base Rarity for the desired Reagent Part");
 
             }
             case 5 ->{
-                descriptor2 = "Shrewd. They know the value and use of many \n" +
+                descriptor2 = "Shrewd. " +
+                        "They know the value and use of many \n" +
                         "things, and enjoy haggling for bargains.";
                 benefit = ("Earn +1 Trinket when trading Remedies for Trinkets.");
-
             }
             case 6 ->{
                 descriptor2 = """
-                        Vigorous. Through the use of leveraging\s
-                        contraptions or perhaps brute strength, your\s
-                        Familiar helps you move many things.""";
+                        Vigorous.\s
+                        Through the use of leveraging contraptions or perhaps brute strength, 
+                        your Familiar helps you move many things.""";
                 benefit = ("Gain +2 Carry (or +4 with a Wagon)");
-
             }
             case 7 ->{
                 descriptor2 = """
-                        Resourceful. Through friendly connection or\s
-                        mysterious means, they have a knack for sourcing\s
-                        a special reagent.""";
+                        Resourceful.\s
+                        Through friendly connection or mysterious means,\s
+                        they have a knack for sourcing a special reagent.""";
                 benefit = ("""
-                        Choose a Reagent (Base Rarity ≤7). Your Familiar\s
-                        allows you to Forage for it in any Region. \s
-                        This Reagent can be changed at the start of every\s
-                        new Journey.""");
-
+                        Choose a Reagent (Base Rarity ≤7). Your Familiar allows you to Forage for it in any Region. \s
+                        This Reagent can be changed at the start of every new Journey.""");
             }
             case 8 ->{
                 descriptor2 = """
-                        Seasoned. They toured the Bristley Woods more\s
-                        times than they care to remember and knows\s
-                        many routes.""";
+                        Seasoned.\s
+                        They toured the Bristley Woods more times than they care to remember and knows many routes.""";
                 benefit = ("When drawing a Travel encounter, you may draw \n" +
                         "twice and choose the card you prefer.");
-
             }
             case 9 ->{
                 descriptor2 = """
-                        Perceptive. They keep a keen eye on changing\s
-                        seasons and know many great spots to forage in\s
-                        for the freshest Reagents.\s""";
+                        Perceptive.\s
+                        They keep a keen eye on changing seasons\s
+                        and know many great spots to forage in for the freshest Reagents.\s""";
                 benefit = ("Start each Ailment with +2 Foraging Points.");
-
             }
             case 10 ->{
                 descriptor2 = """
-                        Independent. Whether a free spirit, easily\s
-                        overwhelmed in crowds or seeking new\s
-                        experiences, they often wander off on their own.\s
-                        However, they always share what they find.""";
+                        Independent.\s
+                        Whether a free spirit, easily overwhelmed in crowds or seeking new experiences,\s
+                        they often wander off on their own. However, they always share what they find.""";
                 benefit = ("""
-                        Once per Ailment, your Familiar can Forage at an\s
-                        Adjacent Location. Draw a card to see what they\s
-                        find. They do not trigger events or reduce Timers.\s""");
-
+                        Once per Ailment, your Familiar can Forage at an Adjacent Location.\s
+                        Draw a card to see what they find. They do not trigger events or reduce Timers.\s""");
             }
             case 11 ->{
                 descriptor2 = """
-                        Titanwise. Your Familiar likes delving into\s
-                        Behemoth barrows and climbing the towering\s
-                        Titan ruins that dot the Bristley Woods—niche but\s
-                        invaluable experiences.""";
+                        Titanwise.\s
+                        Your Familiar likes delving into Behemoth barrows and climbing the towering Titan ruins that dot the Bristley Woods
+                        —niche but invaluable experiences.""";
                 benefit = ("""
-                        -2 TITAN Reagent Rarity. While Foraging Titan and\s
-                        Barrow Locations, you may draw twice and choose\s
-                        the card you prefer.""");
-
+                        -2 TITAN Reagent Rarity. While Foraging Titan and Barrow Locations,\s
+                        you may draw twice and choose the card you prefer.""");
             }
             case 12, 13 ->{
                 descriptor2 = """
-                        Ingenuitive. Your Familiar has cultivated a rather\s
-                        unconventional set of incredibly useful—but\s
-                        impossible to teach—skills.""";
+                        Ingenuitive.\s
+                        Your Familiar has cultivated a rather unconventional set of incredibly useful —but impossible to teach— skills.""";
                 benefit = ("Your Familiar provides the benefits of a Tool");
             }
         }
@@ -322,10 +297,7 @@ public class readyPoultice {
               System.out.println("Monarch = " + descriptor);
               System.out.println("Choose: Capercaillie, Pine Marten");
           }
-
-
       }
-
     }
     public static void save(String characterID) {
         FileWriter save = null;
@@ -355,7 +327,8 @@ public class readyPoultice {
         System.out.print("Select your option: ");
         int option1 =  sc.nextInt();
         switch (option1) {
-            case 1 -> save(characterID);
+            case 1 -> { save(characterID);
+            System.out.println("***Main Menu***");}
             case 2 -> {
                 makeCharacter();
                 happy();
@@ -444,15 +417,14 @@ canSoar = true;
         scanner.nextLine();
         printCard();
         setFamiliarRelationship();
-        System.out.println(familiarRelationship);
     }
     private static String getOrigin() {
         String origin = ("");
         switch (cardSuit) {
-            case 0 -> origin = (" Inspired by a passing Poulticier, and the impression they left.");
-            case 1 -> origin = (" Recruited to help elderly Poulticiers find reagents.");
-            case 2 -> origin = (" Received treatment after a violent encounter.");
-            case 3 -> origin = (" Needed a Poulticier's services after an accident.");
+            case 0 -> origin = ("Inspired by a passing Poulticier, and the impression they left.");
+            case 1 -> origin = ("Recruited to help elderly Poulticiers find reagents.");
+            case 2 -> origin = ("Received treatment after a violent encounter.");
+            case 3 -> origin = ("Needed a Poulticier's services after an accident.");
         }
         return origin;
     }
